bf7b044f9afd2c3d1e2bf731ddfa1fb1e8fb2dcb
Kirill Guryev <k.guryev@btc-s.ru>
2021-09-03 14:10:27 +0300

    Добавил компонент Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,63 @@
 <template>
-    <h1>Hello home</h1>
+  <div>
+    <Form @onSubmit="handleSubmit" />
+    <List @onRemove="handleRemove" :items="notes" />
+  </div>
 </template>
+<script>
+import Form from '@/components/Notes/Form.vue'
+import List from '@/components/Notes/List.vue'
+export default {
+  components: {
+    Form,
+    List
+  },
+  data() {
+    return {
+      notes: [
+        // {
+        //   title: 'Learn Vue 3',
+        //   tags: ['work']
+        // },
+        // {
+        //   title: 'Learn Vue 2',
+        //   tags: ['work', 'home']
+        // },
+        // {
+        //   title: 'Xyita',
+        //   tags: []
+        // }
+      ]
+    }
+  },
+  methods: {
+    handleSubmit(note) {
+      const newNote = {
+        title: note,
+        tags: []
+      }
+      this.notes.push(newNote)
+    },
+    handleRemove(index) {
+      this.notes.splice(index, 1)
+    },
+    getNotes() {
+      const localNotes = localStorage.getItem('notes')
+      if (localNotes) {
+        this.notes = JSON.parse(localNotes)
+      }
+    }
+  },
+  mounted() {
+    this.getNotes()
+  },
+  watch: {
+    notes: {
+      handler(updateLst) {
+        localStorage.setItem('notes', JSON.stringify(updateLst))
+      },
+      deep: true
+    }
+  }
+}
+</script>
